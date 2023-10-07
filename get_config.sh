622b943619908dffc663580b856baac73d1812e6
petruskaa <petruskaakos@gmail.com>
2023-10-08 00:35:32 +0100

    Edit get_config.sh

--- get_config.sh
+++ get_config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CLUSTER_ID=$(cat cluster-id)
+CLUSTER_ID=$(cat cluster-id|tr -d '"')
 curl -X GET \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer ${TF_VAR_do_token}" \

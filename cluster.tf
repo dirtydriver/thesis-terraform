f03eab05c9f98187bc4301fdf7e630a0e3e76e41
petruskaa <petruskaakos@gmail.com>
2023-10-07 23:00:44 +0100

    Update kubernetes cluster version

--- cluster.tf
+++ cluster.tf
@@ -2,7 +2,7 @@ resource "digitalocean_kubernetes_cluster" "gdf_thesis" {
   name   = "gdf-thesis"
   region = "fra1"
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.19.3-do.2"
+  version = "1.19.3-do.3"
 
   node_pool {
     name       = "worker-pool"

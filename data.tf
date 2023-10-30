a4fb67fffe0cb2c18dcee0553e06a16754a76c09
petruskaa <petruskaakos@gmail.com>
2023-10-30 23:20:52 +0100

    Restructure terraform project

---
+++ data.tf
@@ -0,0 +1,14 @@
+ data "terraform_remote_state" "nexus" {
+    backend "artifactory" {
+        # URL of the nexus repository
+        url      = "https://nexus.thepetruska.com/repository/" 
+        # the repository name you just created
+        repo     = "raw_files" 
+        # an unique path to for identification
+        subpath  = "terraform"
+        # an username that has permissions to the repository
+        username = var.backend_user
+        # the password of the username you provided
+        password = var.backend_password 
+        }
+ }
--- version.tf
+++ version.tf
@@ -4,18 +4,5 @@ terraform {
       source = "digitalocean/digitalocean"
       version = "2.3.0"
     }
-    }
-    backend "artifactory" {
-    # URL of the nexus repository
-    url      = "https://nexus.thepetruska.com/repository/" 
-    # the repository name you just created
-    repo     = "raw_files" 
-    # an unique path to for identification
-    subpath  = "terraform"
-    # an username that has permissions to the repository
-    username = var.backend_user
-    # the password of the username you provided
-    password = var.backend_password 
-    }
-    
+    }    
 }

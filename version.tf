3b4341f73d41b8df4df42d6386c1d26404f07c9a
petruskaa <petruskaakos@gmail.com>
2023-10-30 23:50:33 +0100

    Remove username and pass

--- version.tf
+++ version.tf
@@ -4,5 +4,15 @@ terraform {
       source = "digitalocean/digitalocean"
       version = "2.3.0"
     }
-    }    
+      }
+
+    backend "artifactory" {
+      # URL of the nexus repository
+      url      = "https://nexus.thepetruska.com/repository/" 
+      # the repository name you just created
+      repo     = "raw_files" 
+      # an unique path to for identification
+      subpath  = "terraform"
+     
+      }   
 }

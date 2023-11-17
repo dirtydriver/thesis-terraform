c1999d31642ef3e03cdc9b37b97b355ed9c9a23b
Petruska Ákos <petruskaakos@gmail.com>
2023-11-17 23:02:05 +0100

    Add autoscaler to manage kubernetes cluster

--- cluster.tf
+++ cluster.tf
@@ -1,13 +1,14 @@
 resource "digitalocean_kubernetes_cluster" "gdf_thesis" {
   name   = "gdf-thesis"
   region = "fra1"
-  # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.24.4-do.0"
+  version = "1.24.8-do.0"
 
   node_pool {
     name       = "worker-pool"
     size       = "s-2vcpu-2gb"
-    node_count = 3
+    auto_scale = true
+    min_nodes = 2
+    max_nodes = 4
   }
 }
 output "cluster-id" {
